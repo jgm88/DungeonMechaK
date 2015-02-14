4b553b2c374e63d7443d428cdc50e858073b49a7
jgm79 <jgm79@alu.ua.es>
2015-02-14 22:10:45 +0100

    Reorganizadas carpetas y limpiado parcialmente

---
+++ Assets/Scripts/miscelanea/moverCamaraMuerte.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moverCamaraMuerte : MonoBehaviour {
+	
+	
+	public GameObject camaraPrincipal;
+	public GameObject camaraWeapons;
+	public GameObject guitextMuerte;
+
+	private GameObject player;
+	private Material noSkybox;
+	private controladorGuitexts controladorGuitexts;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+		controladorGuitexts = GameObject.FindWithTag("GuitextsContainer").GetComponent<controladorGuitexts>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	//funcion que mueve la camara muerte al morir
+	public void moverCamara(){
+
+		camaraWeapons.camera.enabled = false;
+		controladorGuitexts.enableMensajeMuerte();
+		
+		//desactivo control del pj
+		player.GetComponent<MouseLook>().enabled = false;
+
+		//player.GetComponent("CharacterMotor").SendMessage("canControl", false);
+		player.GetComponent<CharacterMotor>().canControl = false;
+		
+		//eventos itween
+		iTweenEvent.GetEvent(this.gameObject, "moverCamaraMuerte").Play();
+		iTweenEvent.GetEvent(this.gameObject, "rotarCamaraMuerte").Play();
+		iTweenEvent.GetEvent(guitextMuerte, "mostrarMuerte").Play();
+		
+		
+		
+		//cambio los settings de la camara y el render para dar aspecto de muerte
+		
+	}
+
+	
+	
+
+}
