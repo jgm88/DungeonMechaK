46dfa4eea95163f01b20784291e57720da095d87
jgm79 <jgm79@alu.ua.es>
2015-02-17 22:09:27 +0100

    cambiados prefabs de guns y escena jgm

--- Assets/Scripts/Others/Gun.cs
+++ Assets/Scripts/Others/Gun.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Gun : MonoBehaviour {
 	
 	public GameObject bulletPrefab;
-	public Transform spawnPoint;
 	public bool canShot;
 	
 	public float frecuencia = 10;
@@ -12,30 +11,28 @@ public class Gun : MonoBehaviour {
 	public bool firing = false;
 	public float dps = 20f;
 	public float fuerzaPS = 20f;
-	public string boton;
-	public GameObject mecha;
 	public float coolDown = 2f;
 
 	//CACA DE INPUTS
 
 
 
-	
-	
 	void Awake () {
-		if(spawnPoint == null) spawnPoint = transform;
 		canShot = true;
+
 	}
 	public void Shoot(){
 		if(canShot){
 			Quaternion coneRandomRotation = Quaternion.Euler(Random.Range(-conoDispersion, conoDispersion), 
 			                                                 Random.Range(-conoDispersion, conoDispersion),0);
-			Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation * coneRandomRotation);
+			Instantiate(bulletPrefab, transform.position, transform.rotation * coneRandomRotation);
 			canShot = false;
 			StartCoroutine(COCoolDown());
+					
 		}
 
 	}
+
 
 	IEnumerator COCoolDown(){
 		
