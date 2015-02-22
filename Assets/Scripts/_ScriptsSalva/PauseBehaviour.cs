42cc770646421d04160849d51ebb0ca530f7f1a5
Salva <sgm69@alu.ua.es>
2015-02-22 17:38:57 +0100

    realizando comportamiento de pause

---
+++ Assets/Scripts/_ScriptsSalva/PauseBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseBehaviour : MonoBehaviour {
+
+	private MouseLook mouseLook;
+	private bool inPause;
+	// Use this for initialization
+	void Start () {
+		mouseLook = GameObject.FindWithTag("Player").GetComponent<MouseLook>();
+		inPause = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!inPause && Input.GetKeyDown(KeyCode.Escape))
+		{
+			mouseLook.enabled = false;
+			Time.timeScale = 0;
+			inPause = true;
+		}
+		else if (inPause && Input.GetKeyDown(KeyCode.Escape))
+		{
+			mouseLook.enabled = true;
+			Time.timeScale = 1;
+			inPause = false;
+		}
+		
+	}
+}
