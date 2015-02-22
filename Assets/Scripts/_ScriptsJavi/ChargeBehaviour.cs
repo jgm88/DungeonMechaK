76b6503a76dbe9bfff136ff0b7ff1b1999252197
Salva <sgm69@alu.ua.es>
2015-02-22 19:04:49 +0100

    anyadido menu de pause al juego, retocada velocidad con la que recalcula el path el boss

--- Assets/Scripts/_ScriptsJavi/ChargeBehaviour.cs
+++ Assets/Scripts/_ScriptsJavi/ChargeBehaviour.cs
@@ -7,18 +7,20 @@ public class ChargeBehaviour : MonoBehaviour {
 	private WickBehaviour _wickBehaviour;
 	private Image _maskSkill;
 	private PlayerBehaviour _playerBehaviour;
-
+	private LetPowerize _letPowerize;
 	public int manaChargeAmount = 20;
 	public bool canCharge = true;
 	public float coolDown = 0.1f;
 	public bool inWickArea = false;
 //	public GameObject visualEffect;
 	public WickBehaviour wickBehaviour{ set { _wickBehaviour = value;} get { return _wickBehaviour;}}
+	public LetPowerize letPowerize{ set { _letPowerize = value;} get { return _letPowerize;}}
 	public Image maskSkill{ set{ _maskSkill = value;} get {return _maskSkill; }}
 
 	// Use this for initialization
 	void Start () {
 		_playerBehaviour = GameObject.FindWithTag("Player").GetComponent<PlayerBehaviour>();
+
 	}
 
 	public void Charge(){
@@ -26,6 +28,12 @@ public class ChargeBehaviour : MonoBehaviour {
 			_playerBehaviour.ReceiveMana(manaChargeAmount);
 			canCharge = false;
 			_wickBehaviour.currentReload--;
+			StartCoroutine(CoVisualCoolDown());
+		}
+		else if(canCharge && _letPowerize)
+		{
+			_playerBehaviour.ReceiveMana(manaChargeAmount);
+			canCharge = false;
 			StartCoroutine(CoVisualCoolDown());
 		}
 	}
--- Assets/Scripts/_ScriptsSalva/PauseBehaviour.cs
+++ Assets/Scripts/_ScriptsSalva/PauseBehaviour.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class PauseBehaviour : MonoBehaviour {
 
+
 	private MouseLook mouseLook;
 	private bool inPause;
+	private GameObject pauseMenu;
+	private ChangeCursor changeCursor;
 	// Use this for initialization
 	void Start () {
 		mouseLook = GameObject.FindWithTag("Player").GetComponent<MouseLook>();
+		changeCursor = GetComponent<ChangeCursor>();
+		pauseMenu = GameObject.Find("WindowBackground");
+
+		pauseMenu.SetActive(false);
 		inPause = false;
 	}
 	
@@ -15,16 +22,31 @@ public class PauseBehaviour : MonoBehaviour {
 	void Update () {
 		if(!inPause && Input.GetKeyDown(KeyCode.Escape))
 		{
+			ShowCursor(true);
+			pauseMenu.SetActive(true);
 			mouseLook.enabled = false;
 			Time.timeScale = 0;
 			inPause = true;
 		}
 		else if (inPause && Input.GetKeyDown(KeyCode.Escape))
 		{
-			mouseLook.enabled = true;
-			Time.timeScale = 1;
-			inPause = false;
+			QuitPause();
 		}
 		
+	}
+	public void ShowCursor(bool show)
+	{
+		Screen.showCursor = false;
+		Screen.lockCursor = !show;
+		changeCursor.enabled = show;
+	}
+
+	public void QuitPause()
+	{
+		ShowCursor(false);
+		pauseMenu.SetActive(false);
+		mouseLook.enabled = true;
+		Time.timeScale = 1;
+		inPause = false;
 	}
 }
