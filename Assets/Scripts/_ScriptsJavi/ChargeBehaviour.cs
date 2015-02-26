8c29e6fc18fec3009079a48fc741beeae22ff949
Salva <sgm69@alu.ua.es>
2015-02-27 00:26:48 +0100

    anyadida forma en la que cambian las antorchas de color en el boss

--- Assets/Scripts/_ScriptsJavi/ChargeBehaviour.cs
+++ Assets/Scripts/_ScriptsJavi/ChargeBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ChargeBehaviour : MonoBehaviour
 {
@@ -9,18 +10,22 @@ public class ChargeBehaviour : MonoBehaviour
 	private Image _maskSkill;
 	private PlayerBehaviour _playerBehaviour;
 	private LetPowerize _letPowerize;
+	public List<GameObject> colorFlames;
 	public int manaChargeAmount = 20;
 	public bool canCharge = true;
 	public float coolDown = 0.1f;
 	public bool inWickArea = false;
-//	public GameObject visualEffect;
 	public WickBehaviour wickBehaviour{ set { _wickBehaviour = value; } get { return _wickBehaviour; } }
 	public LetPowerize letPowerize{ set { _letPowerize = value; } get { return _letPowerize; } }
 	public Image maskSkill{ set { _maskSkill = value; } get { return _maskSkill; } }
-
+	public GameObject currentFlame;
 	// Use this for initialization
 	void Start ()
 	{
+		currentFlame = colorFlames[0];
+		foreach(var flame in colorFlames)
+			flame.SetActive(false);
+		currentFlame.SetActive(true);
 		_playerBehaviour = GameObject.FindWithTag ("Player").GetComponent<PlayerBehaviour> ();
 
 	}
@@ -33,10 +38,18 @@ public class ChargeBehaviour : MonoBehaviour
 			_wickBehaviour.currentReload--;
 			StartCoroutine (CoVisualCoolDown ());
 		} else if (canCharge && _letPowerize) {
+			activeFlame(_letPowerize.stateActive);
 			_playerBehaviour.ReceiveMana (manaChargeAmount);
 			canCharge = false;
 			StartCoroutine (CoVisualCoolDown ());
 		}
+	}
+
+	private void activeFlame(int state)
+	{
+		currentFlame.SetActive (false);
+		currentFlame = colorFlames[state];
+		currentFlame.SetActive(true);
 	}
 
 	IEnumerator CoVisualCoolDown ()
