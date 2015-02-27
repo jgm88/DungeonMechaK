c199a47a18a14ccf4530ebcbee9a0dd2947bb605
jgm88 <jgm79@outlook.com>
2015-02-27 05:15:22 +0100

    Anyadido efecto de recoger skill1

--- Assets/Scripts/_ScriptsJavi/ChargeBehaviour.cs
+++ Assets/Scripts/_ScriptsJavi/ChargeBehaviour.cs
@@ -10,6 +10,8 @@ public class ChargeBehaviour : MonoBehaviour
 	private Image _maskSkill;
 	private PlayerBehaviour _playerBehaviour;
 	private LetPowerize _letPowerize;
+	private GameObject _currentFlame;
+
 	public List<GameObject> colorFlames;
 	public int manaChargeAmount = 20;
 	public bool canCharge = true;
@@ -18,14 +20,15 @@ public class ChargeBehaviour : MonoBehaviour
 	public WickBehaviour wickBehaviour{ set { _wickBehaviour = value; } get { return _wickBehaviour; } }
 	public LetPowerize letPowerize{ set { _letPowerize = value; } get { return _letPowerize; } }
 	public Image maskSkill{ set { _maskSkill = value; } get { return _maskSkill; } }
-	public GameObject currentFlame;
+	public GameObject visualEffect;
+	public Transform torchTransform;
 	// Use this for initialization
 	void Start ()
 	{
-		currentFlame = colorFlames[0];
+		_currentFlame = colorFlames[0];
 		foreach(var flame in colorFlames)
 			flame.SetActive(false);
-		currentFlame.SetActive(true);
+		_currentFlame.SetActive(true);
 		_playerBehaviour = GameObject.FindWithTag ("Player").GetComponent<PlayerBehaviour> ();
 
 	}
@@ -36,20 +39,21 @@ public class ChargeBehaviour : MonoBehaviour
 			_playerBehaviour.ReceiveMana (manaChargeAmount);
 			canCharge = false;
 			_wickBehaviour.currentReload--;
+			Instantiate(visualEffect, torchTransform.position, torchTransform.rotation);
 			StartCoroutine (CoVisualCoolDown ());
 		} else if (canCharge && _letPowerize) {
 			activeFlame(_letPowerize.stateActive);
 			_playerBehaviour.ReceiveMana (manaChargeAmount);
 			canCharge = false;
+			Instantiate(visualEffect, torchTransform.position, torchTransform.rotation);
 			StartCoroutine (CoVisualCoolDown ());
 		}
 	}
-
 	private void activeFlame(int state)
 	{
-		currentFlame.SetActive (false);
-		currentFlame = colorFlames[state];
-		currentFlame.SetActive(true);
+		_currentFlame.SetActive (false);
+		_currentFlame = colorFlames[state];
+		_currentFlame.SetActive(true);
 	}
 
 	IEnumerator CoVisualCoolDown ()
---
+++ Assets/Scripts/_ScriptsJavi/LifeTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeTime : MonoBehaviour {
+
+	public float lifeTime = 0.5f;
+	public bool hasAudio = false;
+	
+	private float _spawnTime = 0.0f;
+	
+	void OnEnable(){
+		_spawnTime = Time.time;
+		if(hasAudio)
+			audio.Play();
+	}
+	
+	void Update () {
+		
+		if(Time.time > _spawnTime + lifeTime){
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
