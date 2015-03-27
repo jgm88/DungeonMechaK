f51e51ce5f7cf247b2802902ab79255b013c98c4
jgm79 <jgm79@alu.ua.es>
2015-03-27 18:44:00 +0100

    Anyadida decoracion y markers de Minimap con sus prefabs

---
+++ Assets/Scripts/_ScriptsJavi/MarkerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarkerBehaviour : MonoBehaviour {
+
+	private Transform _tPlayer;
+	private Vector3 _vRotation;
+	// Use this for initialization
+	void Start () {
+		_tPlayer = GameObject.FindWithTag("Player").transform;
+		_vRotation = new Vector3(180f,0f,0);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		transform.position = _tPlayer.position;
+		_vRotation.y = _tPlayer.eulerAngles.y;
+
+		Debug.Log(_tPlayer.eulerAngles.y);
+		Debug.Log(_vRotation);
+		transform.eulerAngles = _vRotation;
+//		transform.eulerAngles
+	}
+
+}
