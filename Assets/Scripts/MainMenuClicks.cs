fcf52c3b96251437dbdf9e97a83cb15e66d76939
Toni <antam91@gmail.com>
2015-08-05 13:13:45 +0200

    Cambios en los logos, y añadidos clicks que llevan a URL

---
+++ Assets/Scripts/MainMenuClicks.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuClicks : MonoBehaviour 
+{
+	public Button JuegaTerapiaButton;
+	public Button DMKButton;
+	public Button PlayAndCareButton;
+
+	void Start () 
+	{
+		// JuegaTerapia
+		JuegaTerapiaButton.onClick.AddListener (() => { ClickJuegaTerapia (); });
+
+		// DMK
+		DMKButton.onClick.AddListener (() => { ClickDMK (); });
+
+		// PlayAndCare
+		PlayAndCareButton.onClick.AddListener (() => { ClickPlayAndCare (); });
+	}
+	
+	public void ClickJuegaTerapia ()
+	{
+		Debug.Log ("JuegaTerapiaButton click");
+		Application.OpenURL ("http://www.juegaterapia.org/");
+	}
+	
+	public void ClickDMK ()
+	{
+		Debug.Log ("DMKButton click");
+		Application.OpenURL ("https://www.facebook.com/demekateam");
+	}
+	
+	public void ClickPlayAndCare ()
+	{
+		Debug.Log ("PlayAndCareButton click");
+		//Application.OpenURL ("asdf");
+	}
+}
